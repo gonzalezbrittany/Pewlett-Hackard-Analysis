3598d4d1ce5360330c0a9341b71ced8585b4862d
gonzalezbrittany <gonzalezbrittany2010@gmail.com>
2022-02-09 16:09:56 -0600

    queries

--- schema.sql
+++ schema.sql
@@ -31,14 +31,7 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-CREATE TABLE manager (
-  dept_no VARCHAR(4) NOT NULL,
-  emp_no INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (dept_no)
-);
+
 CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
   dept_no VARCHAR(4) NOT NULL,
@@ -53,8 +46,6 @@ CREATE TABLE title(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES dept_emp (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
 );
-SELECT * FROM departments;
+SELECT * FROM title;
